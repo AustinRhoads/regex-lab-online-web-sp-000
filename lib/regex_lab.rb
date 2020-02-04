cb460a015e19f7d56d31c510ae4dec96f7f817bf
Austin Rhoads <austinrrhoads@ymail.com>
2020-02-04 00:28:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,24 @@
 def starts_with_a_vowel?(word)
-
+rul = word.match(/(\b[aeiouAEIOU][a-z]*)/)
+if rul == nil
+  return false
+else
+return true
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+ text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
+  text.scan(/\b\S{5}\b/)
 
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+rul = text.scan(/^[A-Z].*[!.?]/)
+return rul[0] == text
 end
 
 def valid_phone_number?(phone)
